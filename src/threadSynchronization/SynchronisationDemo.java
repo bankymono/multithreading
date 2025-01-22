23e07865795f06cb18595fcb7399f204534e0b75
Ayodeji Bankole <bankymono@gmail.com>
2025-01-22 16:00:28 +0100

    #

---
+++ src/threadSynchronization/SynchronisationDemo.java
@@ -0,0 +1,40 @@
+package threadSynchronization;
+
+public class SynchronisationDemo {
+    private static int counter1 = 0;
+    private static int counter2 = 0;
+
+    public static void main(String[] args) {
+        Thread one = new Thread(()->{
+            for(int i = 0; i < 10000; i++) {
+                increment1();
+            }
+        });
+
+        Thread two = new Thread(()->{
+            for(int i = 0; i < 10000; i++) {
+                increment2();
+            }
+        });
+
+        one.start();
+        two.start();
+
+        try {
+            one.join();
+            two.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Counter value : " + counter1 + "----" + counter2);
+    }
+
+    private synchronized static void increment1() {
+        counter1++;
+    }
+
+    private synchronized static void increment2() {
+        counter2++;
+    }
+}
